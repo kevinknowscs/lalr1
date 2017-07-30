a61953cf8a1a212b8b3a9cc9ec2c286ef4fee5c5
Kevin Dietz <kevinknowscs@gmail.com>
2017-07-30 09:21:02 -0600

    C# parser generator cleanup

--- src/Lib.Parsing/Grammar/BnfTerm.cs
+++ src/Lib.Parsing/Grammar/BnfTerm.cs
@@ -1,17 +1,15 @@
-﻿#region using
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-#endregion
 
 namespace PGSG.Grammar
 {
   public abstract class BnfTerm
   {
+    // ////////////////////////////////////////////////////////////////////////////////////////////
     // Constructors
+    // ////////////////////////////////////////////////////////////////////////////////////////////
 
-    public BnfTerm(Grammar grammar, string name)
+    protected BnfTerm(Grammar grammar, string name)
     {
       Name = name;
       Guid = Guid.NewGuid();
@@ -21,52 +19,22 @@ namespace PGSG.Grammar
       Follow = new HashSet<Terminal>();
     }
 
+    // ////////////////////////////////////////////////////////////////////////////////////////////
     // Properties
+    // ////////////////////////////////////////////////////////////////////////////////////////////
 
-    public string Name
-    {
-      get;
-      set;
-    }
+    public string Name { get; set; }
 
-    public Guid Guid
-    {
-      get;
-      set;
-    }
+    public Guid Guid { get; set; }
 
-    public Grammar Grammar
-    {
-      get;
-      set;
-    }
+    public Grammar Grammar { get; set; }
 
-    public virtual bool IsEmpty
-    {
-      get
-      {
-        return false;
-      }
-    }
+    public virtual bool IsEmpty => false;
 
-    public virtual bool IsEndOfInput
-    {
-      get
-      {
-        return false;
-      }
-    }
+    public virtual bool IsEndOfInput => false;
 
-    public HashSet<Terminal> First
-    {
-      get;
-      private set;
-    }
+    public HashSet<Terminal> First { get; private set; }
 
-    public HashSet<Terminal> Follow
-    {
-      get;
-      private set;
-    }
+    public HashSet<Terminal> Follow { get; private set; }
   }
 }
